539d28d9b18d341a306ccee12b2b1fbd616c033b
klinga <klingaroo@gmail.com>
2020-10-09 00:08:58 -0400

    base class error added

---
+++ bookops_nypl_platform/errors.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+bookops_nypl_platform.errors
+============================
+This module contains the set of bookops_nypl_platform exceptions.
+"""
+
+
+class BookopsPlatformError(Exception):
+    """Base class for exceptions in bookops_nypl_platform"""
+
+    pass
