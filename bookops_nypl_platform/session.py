6c3672e82b84a2fe5b49f3467dc6f7cc6d49e704
Charlotte Kostelic <charlottekostelic@gmail.com>
2026-05-20 10:37:00 -0700

    Added GET /item endpoint (#15)
    * added get_item method for /items/{nyplSource}/{id}
    
    * updated changelog and exported updated requirements.txt files

--- bookops_nypl_platform/session.py
+++ bookops_nypl_platform/session.py
@@ -113,6 +113,9 @@ class PlatformSession(requests.Session):
     def _get_hold_requests_by_id_url(self, id: Union[str, int]) -> str:
         return f"{self.base_url}/hold-requests/{id}"
 
+    def _get_item_url(self, id: Union[str, int], nyplSource: str) -> str:
+        return f"{self.base_url}/items/{nyplSource}/{id}"
+
     def _get_item_list_url(self) -> str:
         return f"{self.base_url}/items"
 
@@ -371,6 +374,54 @@ class PlatformSession(requests.Session):
         id = self._prep_sierra_number(id)
 
         url = self._get_bib_items_url(id, nyplSource)
+
+        # check if token expired and request new one if needed
+        if self.authorization.is_expired():
+            self._fetch_new_token()
+
+        # send request
+        try:
+            response = self.get(url, timeout=self.timeout, hooks=hooks)
+            return response
+        except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
+            raise BookopsPlatformError(f"Connection error: {sys.exc_info()[0]}")
+        except BookopsPlatformError:
+            raise
+        except Exception:
+            raise BookopsPlatformError(f"Unexpected request error: {sys.exc_info()[0]}")
+
+    def get_item(
+        self,
+        id: Union[str, int],
+        nyplSource: str = "sierra-nypl",
+        hooks: Optional[dict[str, Callable]] = None,
+    ) -> requests.Response:
+        """
+        Requests a specific item record resource using its Sierra item ID.
+
+        Uses GET /items/{nyplSource}/{id} endpoint.
+
+        Args:
+            id:
+                Sierra item ID with or without 'i' prefix and 9th digit check
+            nyplSource:
+                data source; default is 'sierra-nypl'
+            hooks:
+                Requests library hook system that can be used for signal event handling,
+                see more at: https://requests.readthedocs.io/en/master/user/advanced/#event-hooks
+
+        Returns:
+            `requests.Response` object
+        """
+        if not all([id, nyplSource]):
+            raise BookopsPlatformError(
+                "Both arguments `id` and `nyplSource` are required."
+            )
+
+        # verify id
+        id = self._prep_sierra_number(id)
+
+        url = self._get_item_url(id, nyplSource)
 
         # check if token expired and request new one if needed
         if self.authorization.is_expired():
